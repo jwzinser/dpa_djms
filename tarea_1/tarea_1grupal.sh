28398e567d7706831c0a465d183fbadf578dd83b
Songeo <sonia.gmcl@gmail.com>
2017-02-02 19:01:55 -0600

    avance tarea 1 grupo

--- tarea_1/tarea_1grupal.sh
+++ tarea_1/tarea_1grupal.sh
@@ -1,4 +1,19 @@
+# EQUIPO:
+# Mariana Carmona
+# Luis Daniel Hernandez
+# Sonia Mendizabal
+# Juan Zinzer
 
-# Pregunta 1.
+
+# TAREA 1
+# Descarga de archivos
 http http://data.gdeltproject.org/events/index.html | grep -Po '201(612|7..)[0-9][0-9]\.export.CSV.zip' | uniq | awk '{print "http://data.gdeltproject.org/events/"$1}' | parallel wget
+
+# Peso
+peso=$(du -h .)
+echo "La descarga pesa:" $peso
+
+# Numero de archivos
+numarch=$(ls *.zip | wc -l)
+echo "Número de archivos:" $numarch
 
