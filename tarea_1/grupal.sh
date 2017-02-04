f9e2eb9c3529c6af716746b06fc95d47f9cc5f32
luhernan <luhernan@users.noreply.github.com>
2017-02-04 08:46:26 -0600

    Add files via upload
    Ya realiza todo, pero falta que quede en una sola linea

---
+++ tarea_1/grupal.sh
@@ -0,0 +1,13 @@
+# EQUIPO:
+# Mariana Carmona
+# Luis Daniel Hernandez
+# Sonia Mendizabal
+# Juan Zinzer
+
+#!/bin/sh
+
+
+# TAREA 1
+http http://data.gdeltproject.org/events/index.html | grep -Po '201(612|7..)[0-9][0-9]\.export.CSV.zip' | uniq | awk '{print "http://data.gdeltproject.org/events/"$1}' | parallel wget | (echo "Los archivos descargados pesan: `du -h .`"; echo "El numero de archivos descargados es: `ls *.zip | wc -l`") | ls *.zip | parallel -j0 zgrep -e "Mexico" | csvsql --db sqlite:///gdelt.db --no-header-row --table mexico --insert
+
+sql2csv --db sqlite:///gdelt.db --query "select column57 as date, count(*) as events, avg(column31) as goldstein_score from mexico group by date;" | csvsql --db sqlite:///gdelt.db --table mexico_ts --insert
